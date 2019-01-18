936b6e2b4f233e5ca5eeb130cbf377083a46f873
liangz <liangzhen@kdxfilm.com>
2019-01-18 14:27:26 +0800

    LeetCode 3 https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/

---
+++ LeetCode/src/main/java/com/android/leetcode/LeetCode3.java
@@ -0,0 +1,27 @@
+package com.android.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+/**
+ * @CLassName: LeetCode3
+ * @Description: 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ * https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
+ * @author: 梁振
+ * @date: 2019/1/18 2:26 PM
+ *
+ */
+public class LeetCode3 {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>(); // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);
+            }
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j + 1);
+        }
+        return ans;
+    }
+}
