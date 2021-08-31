9d900eaa4768a9a8e6ba0ddd8a773977f835314e
liangz <liangzhen@kdxfilm.com>
2021-08-31 08:57:15 +0800

    Offer 18. 删除链表的节点

---
+++ LeetCode/src/main/java/com/android/leetcode/offer/Offer18.java
@@ -0,0 +1,38 @@
+package com.android.leetcode.offer;
+//
+// Created by $梁振 on 8/31/21.
+//
+
+public class Offer18 {
+    public static void main(String[] args) {
+
+    }
+
+    class Solution {
+        public ListNode deleteNode(ListNode head, int val) {
+            if (head == null) {
+                return null;
+            }
+            if (head.val == val) {
+                return head.next;
+            }
+            ListNode cur = head;
+            while (cur.next != null && cur.next.val != val) {
+                cur = cur.next;
+            }
+            if (cur.next != null) {
+                cur.next = cur.next.next;
+            }
+            return head;
+        }
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
--- LeetCode/src/main/java/com/android/leetcode/offer/Offer47.java
+++ LeetCode/src/main/java/com/android/leetcode/offer/Offer47.java
@@ -1,7 +1,49 @@
 package com.android.leetcode.offer;
-//
-// Created by $梁振 on 8/30/21.
-//
 
+/**
+ * Offer47
+ * TODO
+ *
+ * @author liangzhen
+ * @date 8/30/21 9:45 AM
+ */
 public class Offer47 {
+
+}
+
+class Solution {
+
+    public int maxValue(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0) {
+                    if (j == 0) {
+                        continue;
+                    } else {
+                        grid[0][j] += grid[0][j - 1];
+                    }
+                } else {
+                    if (j == 0) {
+                        grid[i][0] += grid[i - 1][0];
+                    } else {
+                        grid[i][j] += Math.max(grid[i][j - 1], grid[i - 1][j]);
+                    }
+                }
+            }
+        }
+        return grid[m - 1][n - 1];
+    }
+
+    public int maxValue1(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        for (int j = 1; j < n; j++) // 初始化第一行
+            grid[0][j] += grid[0][j - 1];
+        for (int i = 1; i < m; i++) // 初始化第一列
+            grid[i][0] += grid[i - 1][0];
+        for (int i = 1; i < m; i++)
+            for (int j = 1; j < n; j++)
+                grid[i][j] += Math.max(grid[i][j - 1], grid[i - 1][j]);
+        return grid[m - 1][n - 1];
+    }
 }
